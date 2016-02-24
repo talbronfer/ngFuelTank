a6d56c658eb4e623c5142bf91a0271c8061ac82c
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 16:02:31 +0200

    Switched to using inline template instead of templateUrl

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -11,7 +11,7 @@
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            templateUrl: 'fuel-tank.html',
+            template: "<svg height='200'><a><g><circle r='50' fill='#f60'></circle></g><g> <clipPath id='g-clip'> <rect id='g-clip-rect' y='-50' height='50' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
             replace: true,
             scope: {
                 fillPercentage: "="
