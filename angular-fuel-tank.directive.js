9092269bcff7f0f629df2bacdc7b6ab9a1fbe984
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 16:15:53 +0200

    Added dynamic fill percentage calculation

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -11,10 +11,14 @@
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            template: "<svg height='200'><a><g><circle r='50' fill='#f60'></circle></g><g> <clipPath id='g-clip'> <rect id='g-clip-rect' y='-50' height='50' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
+            template: "<svg height='{{height}}'><a><g><circle r='50' fill='#f60'></circle></g><g> <clipPath id='g-clip'> <rect id='g-clip-rect' y='-50' height='{{fillHeight}}' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
             replace: true,
             scope: {
+                height: "=",
                 fillPercentage: "="
+            },
+            link: function(scope) {
+                scope.fillHeight = scope.height / (100 / scope.fillPercentage);
             }
         };
     }
