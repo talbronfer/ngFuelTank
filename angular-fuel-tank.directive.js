48de521c03ecc48076d2c207215f3b6da2933f0d
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 16:59:45 +0200

    Switched to ng-attr syntax

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -18,7 +18,7 @@
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            template: "<svg ng-height='{{height}}'><a><g><circle r='50' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' y='-50' ng-height='{{fillHeight}}' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
+            template: "<svg ng-attr-height='{{height}}'><a><g><circle r='50' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' y='-50' ng-attr-height='{{fillHeight}}' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
             replace: true,
             scope: {
                 height: "=",
