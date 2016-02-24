d74583e30795a4b4441027d5a27e789ae5da15e4
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 16:35:19 +0200

    Added the ng-height directive so that the height compiles before WebKit compiles the SVG template

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -5,13 +5,20 @@
     'use strict';
     angular
         .module('ngFuelTank', [])
-        .directive('fuelTank', ['$document', '$parse', fuelTank]);
+        .directive('fuelTank', ['$document', '$parse', fuelTank])
+        .directive('ngHeight', function() {
+            return function(scope, element, attrs) {
+                scope.$watch(attrs.ngHeight, function(value) {
+                    element.attr('height', value);
+                });
+            };
+        });
 
     function fuelTank($document, $parse) {
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            template: "<svg height='{{height}}'><a><g><circle r='50' fill='#f60'></circle></g><g> <clipPath id='g-clip'> <rect id='g-clip-rect' y='-50' height='{{fillHeight}}' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
+            template: "<svg ng-height='{{height}}'><a><g><circle r='50' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' y='-50' ng-height='{{fillHeight}}' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
             replace: true,
             scope: {
                 height: "=",
