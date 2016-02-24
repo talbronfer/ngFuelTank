2af37a8ed751b9cfbd65036550bdce9ca73fc215
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 19:02:39 +0200

    Changed progress radius to dynamically generated one

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -11,7 +11,7 @@
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            template: "<svg ng-attr-height='{{radius * 2}}'><a><g><circle ng-attr-r='{{radius}}' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' ng-attr-y='{{-radius}}' ng-attr-height='{{(radius * 2) - ((radius * 2)/(100 / fillPercentage))}}' ng-attr-x='{{ -radius}}' ng-attr-width='{{radius * 2}}'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='22' fill='#fff' ></circle> </g> </a> </svg>",
+            template: "<svg ng-attr-height='{{radius * 2}}'><a><g><circle ng-attr-r='{{radius}}' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' ng-attr-y='{{-radius}}' ng-attr-height='{{(radius * 2) - ((radius * 2)/(100 / fillPercentage))}}' ng-attr-x='{{ -radius}}' ng-attr-width='{{radius * 2}}'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='{{ radius - 3}}' fill='#fff' ></circle> </g> </a> </svg>",
             replace: false,
             scope: {
                 radius: '=',
