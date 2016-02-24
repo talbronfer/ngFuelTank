6bdcb05234e36bf71713fd87f722952e1138c31b
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 19:23:11 +0200

    Added hack that solves the SVG's positioning

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -11,7 +11,7 @@
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            template: "<svg ng-attr-height='{{radius * 2}}'><a><g><circle ng-attr-r='{{radius}}' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' ng-attr-y='{{-radius}}' ng-attr-height='{{(radius * 2) - ((radius * 2)/(100 / fillPercentage))}}' ng-attr-x='{{ -radius}}' ng-attr-width='{{radius * 2}}'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='{{ radius - 3}}' fill='#fff' ></circle> </g> </a> </svg>",
+            template: "<svg ng-attr-height='{{radius * 2}}'><a><g><circle ng-attr-r='{{radius}}' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' ng-attr-y='{{-radius}}' ng-attr-height='{{(radius * 2) - ((radius * 2)/(100 / fillPercentage))}}' ng-attr-x='{{ -radius}}' ng-attr-width='{{radius * 2}}'> </rect> </clipPath> <circle clip-path='url(#g-clip)' ng-attr-r='{{ radius - 3}}' fill='#fff' ></circle> </g> </a> </svg>",
             replace: false,
             scope: {
                 radius: '=',
@@ -19,7 +19,8 @@
             },
             compile: function(elem, attrs) {
                 return function(scope) {
-                    //scope.fillHeight = 100 - scope.fillPercentage;
+                    // Temporary hack that positions the SVG correctly in relation to container div
+                    elem.style.cursor = "pointer";
                 };
             }
         };
