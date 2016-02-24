81ee754639306f0804398b6eea49beb2f462f8e0
Tal Bronfer <tal.bronfer@safebreach.com>
2016-02-24 18:49:46 +0200

    Added variable radius

--- angular-fuel-tank.directive.js
+++ angular-fuel-tank.directive.js
@@ -11,10 +11,10 @@
         return {
             restrict: 'E',
             templateNamespace: 'svg',
-            template: "<svg ng-attr-height='{{height}}'><a><g><circle r='50' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' y='-50' ng-attr-height='{{100 - fillPercentage}}' x='-50' width='100'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='47' fill='#fff' ></circle> </g> </a> </svg>",
+            template: "<svg ng-attr-height='{{radius * 2}}'><a><g><circle ng-attr-r='{{radius}}' fill='#f60'></circle></g><g><clipPath id='g-clip'><rect id='g-clip-rect' ng-attr-y='{{-radius}}' ng-attr-height='{{(radius * 2) - ((radius * 2)/(100 / fillPercentage))}}' ng-attr-x='{{ -radius}}' ng-attr-width='{{radius * 2}}'> </rect> </clipPath> <circle clip-path='url(#g-clip)' r='22' fill='#fff' ></circle> </g> </a> </svg>",
             replace: false,
             scope: {
-                height: "=",
+                radius: '=',
                 fillPercentage: "="
             },
             compile: function(elem, attrs) {
